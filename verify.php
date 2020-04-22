6d12e8613a77172adc00ec6f059bfae9d04cfdfb
linebot-naja <47289501+pantawit@users.noreply.github.com>
2020-04-22 22:37:06 +0700

    Update verify.php

--- verify.php
+++ verify.php
@@ -14,18 +14,3 @@ $result = curl_exec($ch);
 curl_close($ch);
 
 echo $result;
-if (!function_exists('http_response_code'))
-{
-    function http_response_code($newcode = NULL)
-    {
-        static $code = 200;
-        if($newcode !== NULL)
-        {
-            header('X-PHP-Response-Code: '.$newcode, true, $newcode);
-            if(!headers_sent())
-                $code = $newcode;
-        }       
-        return $code;
-    }
-}
-?php>
