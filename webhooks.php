e9505e818f98a8b980a973a04678dd1276f23877
pantawit <47289501+pantawit@users.noreply.github.com>
2020-04-22 19:32:03 +0700

    Update webhooks.php

--- webhooks.php
+++ webhooks.php
@@ -3,7 +3,7 @@
 require "vendor/autoload.php";
 require_once('vendor/linecorp/line-bot-sdk/line-bot-sdk-tiny/LINEBotTiny.php');
 
-$access_token = '3ALKAbKFoGuJyJnoDdn0HeyfbxLFtEXBKiC0lFeoNl/XbL4WhoCZzefp2n7UDuXaCWfErIDro07BnZNggJmXJChXTIlMPo8LRJ+n1LEgbRUaKehDkiCr5p5CakHrPX+gauOGX/R5bB2e5yi7xjnHDAdB04t89/1O/w1cDnyilFU=';
+$access_token = 's55lZIbFjuV/+Xp+4RuYzZ67M1qAuMUmxEZI9IAF8nNA+qn3jCXaTRxZC+bXKGGTyAEHRfmZu/OPX4BXs+ldoi8AQY3rBOLNHabaCiE5fCzlch01HuZbBBa1rWsbjNmBvm9SDCfCHL9fGQi4iR5AcgdB04t89/1O/w1cDnyilFU=';
 
 // Get POST body content
 $content = file_get_contents('php://input');
